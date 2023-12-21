8da21bea9ea2df049612b788f09a4c12908be70d
Tossan99 <>
2023-12-21 00:22:34 +0000

    Add comment class model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-tossan99-django-blog-bb3m0yfcst.us2.codeanyapp.com', '.herokuapp.com']
 
